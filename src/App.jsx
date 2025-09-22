973b76d0e150bf61c40a60a2e425585eb6239518
Phnix01 <fomar9235@gmail.com>
2025-09-22 20:57:11 +0100

    Add click handler to Square component and update value props

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function Square({valeur}) {
   function handleClick(){
     alert(' You click on '+{valeur}+' case');
   }
-  return <button className="square">{valeur}</button>;
+  return <button className="square" onClick={handleClick}>{valeur}</button>;
 }
 
 
@@ -14,21 +14,21 @@ export default function Board (){
 
     <>
         <div className='board-row'>
-            <Square valeur={1}/>
-            <Square valeur={1}/>
-            <Square valeur={1}/>
+            <Square valeur="1"/>
+            <Square valeur="2"/>
+            <Square valeur="3"/>
         </div>
 
         <div className='board-row'>
-            <Square valeur={1}/>
-            <Square valeur={1}/>
-            <Square valeur={1}/>
+            <Square valeur="4"/>
+            <Square valeur="5"/>
+            <Square valeur="6"/>
         </div>
 
         <div className='board-row'>
-            <Square valeur={1}/>
-            <Square valeur={1}/>
-            <Square valeur={1}/>
+            <Square valeur="7"/>
+            <Square valeur="8"/>
+            <Square valeur="9"/>
         </div>
     </>
   );
