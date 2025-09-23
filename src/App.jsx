f26cda8d06c5dd1a7b375240e863a35e0c2cf7b2
Phnix01 <fomar9235@gmail.com>
2025-09-23 10:54:31 +0100

    Refactor Square component to manage its own state and remove value prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import './App.css'
-
-function Square({valeur}) {
-
+import { useState } from 'react';
+function Square() {
+const [valeur, setValeur] = useState(null);
   function handleClick(){
-    alert(' You click on '+{valeur}+' case');
+    alert(' You click on '+valeur+' case');
   }
   return <button className="square" onClick={handleClick}>{valeur}</button>;
 }
@@ -14,21 +14,21 @@ export default function Board (){
 
     <>
         <div className='board-row'>
-            <Square valeur="1"/>
-            <Square valeur="2"/>
-            <Square valeur="3"/>
+            <Square />
+            <Square />
+            <Square />
         </div>
 
         <div className='board-row'>
-            <Square valeur="4"/>
-            <Square valeur="5"/>
-            <Square valeur="6"/>
+            <Square />
+            <Square />
+            <Square />
         </div>
 
         <div className='board-row'>
-            <Square valeur="7"/>
-            <Square valeur="8"/>
-            <Square valeur="9"/>
+            <Square />
+            <Square />
+            <Square />
         </div>
     </>
   );
