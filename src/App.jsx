b214fd88ddf52b7ecdbe9682530082a841dc115b
Phnix01 <fomar9235@gmail.com>
2025-09-23 12:07:50 +0100

    Refactor Square component to accept value prop and update rendering in Board component

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,34 @@
 import './App.css'
 import { useState } from 'react';
-function Square() {
-const [valeur, setValeur] = useState(null);
-  function handleClick(){
-    setValeur('X');
-  }
-  return <button className="square" onClick={handleClick}>{valeur}</button>;
+function Square({valeur,squareOnClick}) {
+
+
+  return <button className="square" onClick={squareOnClick}>{valeur}</button>;
 }
 
 
 export default function Board (){
 
-  
+  const [squares, setSquares] =useState(Array(9).fill(null));
   return(
 
     <>
         <div className='board-row'>
-            <Square />
-            <Square />
-            <Square />
+            <Square valeur={squares[0]}/>
+            <Square valeur={squares[1]}/>
+            <Square valeur={squares[2]}/>
         </div>
 
         <div className='board-row'>
-            <Square />
-            <Square />
-            <Square />
+            <Square valeur={squares[3]}/>
+            <Square valeur={squares[4]}/>
+            <Square valeur={squares[5]}/>
         </div>
 
         <div className='board-row'>
-            <Square />
-            <Square />
-            <Square />
+            <Square valeur={squares[6]}/>
+            <Square valeur={squares[7]}/>
+            <Square valeur={squares[8]}/>
         </div>
     </>
   );
