db557fbf323b7af4ec3d5911402a616c8f81276b
Phnix01 <fomar9235@gmail.com>
2025-09-24 14:20:26 +0100

    Update handleClick function to accept index parameter and modify squareOnClick prop in Square component

--- src/App.jsx
+++ src/App.jsx
@@ -11,21 +11,21 @@ export default function Board (){
 
    const [squares, setSquares] = useState(Array(9).fill(null));
 
-  function handleClick(){
+  function handleClick(i){
 
     // la fonction handleClick crée une copie du tableau squres (nextSquares) 
     // grâce à la fonction javascript slice(). ensuite handleClik met à jour le tableau nextSquares pour ajouter un x à la case (index[0])
     const nextSquares = squares.slice();
-    nextSquares[0] = "x";
+    nextSquares[i] = "x";
     setSquares(nextSquares);
   }
   return(
 
     <>
         <div className='board-row'>
-            <Square valeur={squares[0]} onClick={handleClick}/>
-            <Square valeur={squares[1]} onClick={handleClick}/>
-            <Square valeur={squares[2]} onClick={handleClick}/>
+            <Square valeur={squares[0]} squareOnClick={()=>handleClick(0)}/>
+            <Square valeur={squares[1]} squareOnClick={()=>handleClick(1)}/>
+            <Square valeur={squares[2]} squareOnClick={()=>handleClick(2)}/>
         </div>
 
         <div className='board-row'>
